f391c98e8505e9ca71f98077e2a71cc1f4194003
Constanza Lancieri <constanza.lancieri@brubank.com>
2025-05-20 19:30:35 -0300

    create camera position for map

--- CityApp/Views/Map/CitiesMapView.swift
+++ CityApp/Views/Map/CitiesMapView.swift
@@ -10,26 +10,41 @@ import MapKit
 
 struct CitiesMapView: View {
     let city: CitiesListModel
-    @State private var cameraPosition: MapCameraPosition = .region(.init(center: CLLocationCoordinate2D(latitude: -34.603851, longitude: -58.381775), latitudinalMeters: 1300, longitudinalMeters: 1300))
-    
+    @State private var cameraPosition: MapCameraPosition = .automatic
+    private let defaultZoom: Double = 1300
     
     var body: some View {
         VStack {
-            let coordinate = CLLocationCoordinate2D(latitude: city.city.coord.lat, longitude: city.city.coord.lon)
-            
             Map(position: $cameraPosition) {
-                Marker(city.city.name, coordinate: coordinate)
+                Marker(city.city.name, coordinate: cityCoordinate)
             }
             .onChange(of: city) { _, newCity in
                 withAnimation {
-                    cameraPosition = .region(.init(center: CLLocationCoordinate2D(latitude: newCity.city.coord.lat, longitude: newCity.city.coord.lon), latitudinalMeters: 1300, longitudinalMeters: 1300))
+                    cameraPosition = createCameraPosition()
                 }
             }
         }
         .onAppear {
             withAnimation {
-                cameraPosition = .region(.init(center: CLLocationCoordinate2D(latitude: city.city.coord.lat, longitude: city.city.coord.lon), latitudinalMeters: 1300, longitudinalMeters: 1300))
+                cameraPosition = createCameraPosition()
             }
         }
+    }
+    
+    var cityCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: city.city.coord.lat,
+            longitude: city.city.coord.lon
+        )
+    }
+    
+    private func createCameraPosition() -> MapCameraPosition {
+        return .region(
+            MKCoordinateRegion(
+                center: cityCoordinate,
+                latitudinalMeters: defaultZoom,
+                longitudinalMeters: defaultZoom
+            )
+        )
     }
 }
